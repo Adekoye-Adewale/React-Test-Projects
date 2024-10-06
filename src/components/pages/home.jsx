06bc42faf7d25599d0bfd28dabd0a1cc6bc8480d
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-06 21:13:17 +0100

    Create home.jsx

---
+++ src/components/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HomeHeader } from '../headers'
+
+export default function HomePage() {
+        return (
+                <div>
+                        <HomeHeader/>
+                </div>
+        )
+}
