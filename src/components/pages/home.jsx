2e5f7ce30b5d45c66449e9b002450551c6e0679b
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-08 13:00:55 +0100

    Chores: Added Task List component

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { HomeHeader } from '../headers'
+import TaskList, { Filter } from '../homePage/taskList'
 
 export default function HomePage() {
         return (
                 <div>
                         <HomeHeader/>
+                        <div className='py-10 px-5'>
+                                <TaskList/>
+                        </div>
                 </div>
         )
 }
