658cb05b10940b06e033ce34a347906fbe125189
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-12 23:28:44 +0100

    Create page.jsx

---
+++ src/app/landing-page/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LandingPageComponent from '@/components/pages/landingPage'
+
+export default function LandingPage() {
+        return (
+                <LandingPageComponent/>
+        )
+}
