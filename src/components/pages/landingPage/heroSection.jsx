61ba00bcc1c611e3890d3b4f0f57c8cb6dc7b46e
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-18 22:51:14 +0100

    Update heroSection.jsx

--- src/components/pages/landingPage/heroSection.jsx
+++ src/components/pages/landingPage/heroSection.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function HeroSection() {
         return (
-                <section>
-                        Hero Section
+                <section 
+                        className='bg-sky-700 min-h-[50vh] py-20 px-5'
+                >
+                        <Content/>
                 </section>
+        )
+}
+
+const Content = () => {
+        return (
+                <div>
+                        <h1 className='text-3xl font-bold text-white'>
+                                Welcome to our website
+                        </h1>
+                        <p>
+                                This is a paragraph of text
+                        </p>
+                </div>
         )
 }
