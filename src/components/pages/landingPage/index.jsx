c42d13a844214b53093924a4127956ff9f7e506e
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-25 18:08:41 +0100

    Update index.jsx

--- src/components/pages/landingPage/index.jsx
+++ src/components/pages/landingPage/index.jsx
@@ -3,16 +3,20 @@ import NavBar from './navBar'
 import HeroSection from './heroSection'
 import CaseStudySection from './caseStudySection'
 import TestimonialsSection from './testimonialsSection'
+import SkillsSection from './skillsSection'
+import GetInTouch from './getInTouch'
 
 export default function LandingPageComponent() {
         return (
-                <div>
+                <>
                         <NavBar/>
                         <main>
                                 <HeroSection/>
                                 <CaseStudySection/>
                                 <TestimonialsSection/>
+                                <SkillsSection/>
+                                <GetInTouch/>
                         </main>
-                </div>
+                </>
         )
 }
