f098e0ce1927b418f9e9eaffbb83b28a2f751249
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-10-28 12:37:18 +0100

    Create ThemeContext.js

---
+++ src/components/pages/landingPage/ThemeContext.js
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from 'react';
+
+const ThemeContext = createContext({
+        theme: 'light',
+        toggleTheme: () => { },
+});
+
+export function ThemeProvider({ children }) {
+        const [theme, setTheme] = useState(() => {
+                const storedTheme = localStorage.getItem('theme');
+                return storedTheme
+                        ? storedTheme : 'light';
+        });
+
+        const toggleTheme = () => {
+                const newTheme = theme === 'light' ? 'dark' : 'light';
+                setTheme(newTheme);
+                localStorage.setItem('theme', newTheme);
+        };
+
+        useEffect(() => {
+                document.body.className = theme;
+        }, [theme]);
+
+        return (
+                <ThemeContext.Provider 
+                        value={{
+                                theme,
+                                toggleTheme
+                        }}
+                >
+                        {children}
+                </ThemeContext.Provider>
+        );
+}
+
+export {
+        ThemeContext, ThemeProvider
+};
