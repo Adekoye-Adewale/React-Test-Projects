355c62cf89f26423aa64f8ecf0a1e06fddaff2e6
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-02 15:53:07 +0100

    Create components.jsx

---
+++ src/components/pages/landingPage/components.jsx
@@ -0,0 +1,21 @@
+import NavBar from './navBar'
+import HeroSection from './heroSection'
+import CaseStudySection from './caseStudySection'
+import TestimonialsSection from './testimonialsSection'
+import SkillsSection from './skillsSection'
+import GetInTouch from './getInTouch'
+
+export default function Components() {
+        return (
+                <div className={`text-slate-950 dark:text-sky-50`}>
+                        <NavBar />
+                        <main>
+                                <HeroSection />
+                                <CaseStudySection />
+                                <TestimonialsSection />
+                                <SkillsSection />
+                                <GetInTouch />
+                        </main>
+                </div>
+        )
+}
