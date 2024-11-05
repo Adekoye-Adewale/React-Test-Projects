3744781f9640586e9d6726bd2b00055caaff0be5
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-05 22:49:12 +0100

    Update index.jsx

--- src/components/pages/landingPage/index.jsx
+++ src/components/pages/landingPage/index.jsx
@@ -1,22 +1,12 @@
+'use client'
 import React from 'react'
-import NavBar from './navBar'
-import HeroSection from './heroSection'
-import CaseStudySection from './caseStudySection'
-import TestimonialsSection from './testimonialsSection'
-import SkillsSection from './skillsSection'
-import GetInTouch from './getInTouch'
+import { ThemeProvider } from './ThemeContext'
+import Components from './components'
 
 export default function LandingPageComponent() {
         return (
-                <>
-                        <NavBar/>
-                        <main>
-                                <HeroSection/>
-                                <CaseStudySection/>
-                                <TestimonialsSection/>
-                                <SkillsSection/>
-                                <GetInTouch/>
-                        </main>
-                </>
+                <ThemeProvider>
+                        <Components/>
+                </ThemeProvider>
         )
 }
