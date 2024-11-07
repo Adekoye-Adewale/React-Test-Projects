f10e9afc259b63f57beb6cf2f8ace7d3e36915df
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-07 21:54:44 +0100

    Update getInTouch.jsx

--- src/components/pages/landingPage/getInTouch.jsx
+++ src/components/pages/landingPage/getInTouch.jsx
@@ -30,11 +30,14 @@ const LabelContent = () => {
 
 const Form = () => {
         return (
-                <form className='grid gap-2 mx-auto mt-10 max-w-96 text-sky-50'>
+                <form 
+                        className='grid gap-2 mx-auto mt-10 max-w-96 text-sky-50'
+                        action='/'
+                >
                         <span className='grid'>
                                 <label 
-                                        for="email" 
-                                        class="block text-2xl font-normal"
+                                        htmlFor="email" 
+                                        className="block text-2xl font-normal"
                                 >
                                         Email
                                 </label>
@@ -42,16 +45,16 @@ const Form = () => {
                                         type="email" 
                                         name="email" 
                                         id="email" 
-                                        autocomplete="email" 
-                                        class="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-10 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm" 
+                                        autoComplete="email" 
+                                        className="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-10 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm" 
                                         placeholder="janesmith@sample.com"
                                         required
                                 />
                         </span>
                         <span className='grid'>
                                 <label
-                                        for="phone"
-                                        class="block text-2xl font-normal"
+                                        htmlFor="phone"
+                                        className="block text-2xl font-normal"
                                 >
                                         Phone Number
                                 </label>
@@ -60,26 +63,24 @@ const Form = () => {
                                         min={10}
                                         name="phone"
                                         id="phone"
-                                        autocomplete="phone"
-                                        class="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-10 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm"
+                                        autoComplete="phone"
+                                        className="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-10 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm"
                                         placeholder="2349012340987"
                                         required
                                 />
                         </span>
                         <span className='grid'>
                                 <label
-                                        for="message"
-                                        class="block text-2xl font-normal"
+                                        htmlFor="message"
+                                        className="block text-2xl font-normal"
                                 >
                                         Message
                                 </label>
                                 <textarea 
-                                        name="message" 
+                                        name="message"
                                         id="message"
-                                        class="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-32 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm"
-                                >
-                                        Message
-                                </textarea>
+                                        className="block flex-1 border-2 border-sky-200 rounded-md bg-transparent focus-within:text-slate-950 focus-within:bg-sky-50 py-1.5 px-2 w-full h-32 mt-2 placeholder:text-gray-400 focus:outline-0 valid:border-green-700 invalid:border-red-700 sm:text-sm"
+                                />
                         </span>
                         <input 
                                 type="button" 
