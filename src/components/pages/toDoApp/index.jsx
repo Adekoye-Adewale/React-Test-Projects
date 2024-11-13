aa8e50c6fab7140fc1d33fff638e1a3d67a879fd
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-13 18:53:55 +0100

    Create index.jsx

---
+++ src/components/pages/toDoApp/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoApp from './app'
+
+export default function ToDoApp() {
+        return (
+                <>
+                        <TodoApp/>
+                </>
+        )
+}
